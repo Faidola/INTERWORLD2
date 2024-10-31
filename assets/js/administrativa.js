a1b4dae77071549765d7c90546a5b3adc41579fb
Faidola <contato.rafaleite@gmail.com>
2024-10-31 13:50:50 -0300

    finalizado para o video

--- assets/js/administrativa.js
+++ assets/js/administrativa.js
@@ -6,13 +6,17 @@ async function listarIntercambistas() {
     try {
         const resposta = await fetch('http://localhost:5000/intercambistas');
         const intercambistas = await resposta.json();
+        //const senhaMasked = '*'.repeat(intercambista.senha.length); // Cria uma máscara com asteriscos
 
         intercambistas.forEach(intercambista => {
+            const tamanhoMascaraSenha = 10;
+            const senhaMasked = '*'.repeat(tamanhoMascaraSenha);
             const tr = document.createElement('tr');
             tr.innerHTML = `
                 <td>${intercambista.id}</td>
                 <td>${intercambista.nome}</td>
                 <td>${intercambista.email}</td>
+                <td>${senhaMasked}</td>
                 <td>
                     <button onclick="editarIntercambista(${intercambista.id})">Editar</button>
                     <button onclick="deletarIntercambista(${intercambista.id})">Deletar</button>
@@ -26,7 +30,7 @@ async function listarIntercambistas() {
 }
 
 // Chama a função quando a página é carregada
-window.onload = listarIntercambistas;
+window.onload = listarIntercambistas();
 
 async function deletarIntercambista(id) {
     if (confirm('Tem certeza que deseja deletar este intercambista?')) {
@@ -43,5 +47,33 @@ async function deletarIntercambista(id) {
         } catch (error) {
             console.error('Erro:', error);
         }
+    }
+}
+
+async function editarIntercambista(id) {
+    const nome = prompt('Digite o novo nome:');
+    const email = prompt('Digite o novo email:');
+    const senha = prompt('Digite a nova senha:');
+
+    if (nome && email && senha) {
+        try {
+            const resposta = await fetch(`http://localhost:5000/intercambistas/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ nome, email, senha })
+            });
+
+            if (!resposta.ok) {
+                throw new Error('Erro ao editar intercambista');
+            }
+
+            await listarIntercambistas();
+        } catch (error) {
+            console.error('Erro:', error);
+        }
+    } else {
+        alert('Todos os campos devem ser preenchidos.');
     }
 }
