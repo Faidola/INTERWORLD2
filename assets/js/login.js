0d930780fa61230d760ce13ed00e0e1c420cacbd
Faidola <contato.rafaleite@gmail.com>
2024-10-30 22:02:19 -0300

    Ajustes de layout de pagina admin

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,39 @@
 document.getElementById('loginButton').addEventListener('click', function() {
     window.location.href = '../index.html';
 });
+
+document.getElementById('form-cadastro').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const dados = {
+        nome,
+        email,
+        senha
+    };
+
+
+        try {
+            const resposta = await fetch('https://localhost:5000/escolas', { // Substitua pela URL da sua API
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dados)
+            });
+    
+            if (!resposta.ok) {
+                throw new Error('Erro ao cadastrar: ' + resposta.statusText);
+            }
+    
+            const resultado = await resposta.json();
+            document.getElementById('mensagem').textContent = 'Cadastro realizado com sucesso!';
+            // Aqui você pode redirecionar o usuário ou fazer outra ação
+        } catch (error) {
+            document.getElementById('mensagem').textContent = error.message;
+        }
+   
+});
